e1ddfe1dd09c179d03617af330a6cd444006831b
Fallbrawl <herdemismyrise.bt@gmail.com>
2017-09-22 18:08:06 +0300

    limit offset

--- app/src/main/java/com/attracttest/attractgroup/sqlitetask/DBHelper.java
+++ app/src/main/java/com/attracttest/attractgroup/sqlitetask/DBHelper.java
@@ -1,15 +1,21 @@
 package com.attracttest.attractgroup.sqlitetask;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by nexus on 20.09.2017.
  */
 public class DBHelper extends SQLiteOpenHelper {
 
+    // connectin' to bd
+    private SQLiteDatabase db = getWritableDatabase();
 
     public DBHelper(Context context) {
         super(context, "myDB", null, 1);
@@ -33,4 +39,77 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
+    public void add(CustomClass cc) {
+
+        // Creating an object for data
+        ContentValues cv = new ContentValues();
+
+        //Log.d(LOG_TAG, "--- Insert in mytable: ---");
+        // preparing data: column - data
+
+            cv.put("name", cc.getName());
+            cv.put("surname", cc.getSurname());
+            cv.put("date", cc.getDate());
+            cv.put("desc", cc.getDesc());
+            cv.put("misc", cc.getMisc());
+
+            // gettin id ID
+            long rowID = db.insert("mytable", null, cv);
+            //Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+
+        }
+
+    public ArrayList<CustomClass> get(int from, int to, String sort) {
+        ArrayList<CustomClass> result = new ArrayList<>();
+
+        // querin' all database; get Cursor object
+        Cursor c = db.query("mytable", null, null, null, null, null, null,"20," + from);
+
+        // set Cursors position to first
+        // if no values - returnin' false
+        if (c.moveToFirst()) {
+
+            // gettin numbers of columns
+            int idColIndex = c.getColumnIndex("id");
+            int nameColIndex = c.getColumnIndex("name");
+            int surnameColIndex = c.getColumnIndex("surname");
+            int dateColIndex = c.getColumnIndex("date");
+            int descColIndex = c.getColumnIndex("desc");
+            int miscColIndex = c.getColumnIndex("misc");
+            c.moveToPosition(from);
+
+            do {
+                // gettin' values via columns
+//                Log.d(LOG_TAG,
+//                        "ID = " + c.getInt(idColIndex) +
+//                                ", name = " + c.getString(nameColIndex) +
+//                                ", email = " + c.getString(surnameColIndex) +
+//                                ", date = " + c.getString(dateColIndex) +
+//                                ", desc = " + c.getString(descColIndex) +
+//                                ", misc = " + c.getString(miscColIndex));
+
+                result.add(new CustomClass(c.getString(nameColIndex), c.getString(surnameColIndex),
+                        c.getString(dateColIndex),c.getString(descColIndex),
+                        c.getString(miscColIndex)));
+
+                 //movin to the next row
+                // if no next - get out of cycle
+            } while (c.moveToNext());
+        } else
+            Log.d("wow", "0 rows");
+        c.close();
+        return result;
+
+    }
+
+
+
+    @Override
+    public void close () {
+        db.close();
+        super.close();
+    }
+
 }
+
--- app/src/main/java/com/attracttest/attractgroup/sqlitetask/MainActivity.java
+++ app/src/main/java/com/attracttest/attractgroup/sqlitetask/MainActivity.java
@@ -31,73 +31,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ArrayList<CustomClass> classesForDb = CustomClass.init();
+
         classesFromDb = new ArrayList<>();
         final ArrayList<CustomClass> current = new ArrayList<>();
 
-        // Creating an object for data
-        ContentValues cv = new ContentValues();
-
         // Object for controllin' the DB
         dbHelper = new DBHelper(this);
-
-        // подключаемся к БД
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        Log.d(LOG_TAG, "--- Insert in mytable: ---");
-        // подготовим данные для вставки в виде пар: наименование столбца - значение
-
-        for (CustomClass cc :
-                classesForDb) {
-            cv.put("name", cc.getName());
-            cv.put("surname", cc.getSurname());
-            cv.put("date", cc.getDate());
-            cv.put("desc", cc.getDesc());
-            cv.put("misc", cc.getMisc());
-
-            // вставляем запись и получаем ее ID
-            long rowID = db.insert("mytable", null, cv);
-            Log.d(LOG_TAG, "row inserted, ID = " + rowID);
-        }
-
-        Log.d(LOG_TAG, "--- Rows in mytable: ---");
-        // делаем запрос всех данных из таблицы mytable, получаем Cursor
-        Cursor c = db.query("mytable", null, null, null, null, null, null);
-
-        // ставим позицию курсора на первую строку выборки
-        // если в выборке нет строк, вернется false
-        if (c.moveToFirst()) {
-
-            // определяем номера столбцов по имени в выборке
-            int idColIndex = c.getColumnIndex("id");
-            int nameColIndex = c.getColumnIndex("name");
-            int surnameColIndex = c.getColumnIndex("surname");
-            int dateColIndex = c.getColumnIndex("date");
-            int descColIndex = c.getColumnIndex("desc");
-            int miscColIndex = c.getColumnIndex("misc");
-
-            do {
-                // получаем значения по номерам столбцов и пишем все в лог
-                Log.d(LOG_TAG,
-                        "ID = " + c.getInt(idColIndex) +
-                                ", name = " + c.getString(nameColIndex) +
-                                ", email = " + c.getString(surnameColIndex) +
-                                ", date = " + c.getString(dateColIndex) +
-                                ", desc = " + c.getString(descColIndex) +
-                                ", misc = " + c.getString(miscColIndex));
-
-                classesFromDb.add(new CustomClass(c.getString(nameColIndex), c.getString(surnameColIndex),
-                        c.getString(dateColIndex),c.getString(descColIndex),
-                        c.getString(miscColIndex)));
-
-                // переход на следующую строку
-                // а если следующей нет (текущая - последняя), то false - выходим из цикла
-            } while (c.moveToNext());
-        } else
-            Log.d(LOG_TAG, "0 rows");
-        c.close();
-        // закрываем подключение к БД
-        dbHelper.close();
 
         customClassAdapter = new CustomClassAdapter(this, current);
 
@@ -117,15 +56,18 @@ public class MainActivity extends AppCompatActivity {
                     i++;
                     if (totalItemCount % 20 == 0) {
 
-                        current.addAll(classesFromDb.subList(totalItemCount, totalItemCount + 20));
+                        //current.addAll(dbHelper.get().subList(totalItemCount, totalItemCount + 20));
+                        current.addAll(dbHelper.get(totalItemCount, totalItemCount, null));
 
                         left = MAXRECORDS - 20 * i;
 
                     }
 
                     if (left != 0) {
-                        current.addAll(classesFromDb.subList(MAXRECORDS - left, MAXRECORDS));
+                        current.addAll(dbHelper.get(MAXRECORDS - left, MAXRECORDS - left, null));
                     }
+//                    int pos=totalItemCount + 20;
+//                    current.addAll(classesFromDb.subList(totalItemCount, pos<=classesFromDb.size()?pos:classesFromDb.size()));
                     customClassAdapter.notifyDataSetChanged();
                 }
             }
@@ -156,6 +98,13 @@ public class MainActivity extends AppCompatActivity {
                 data.getStringExtra("date"), data.getStringExtra("misc"), data.getStringExtra("desc"));
 
         customClassAdapter.add(whatToAdd);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
 
     }
 }
